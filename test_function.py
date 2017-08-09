26c665b8362c8b13427319a2272c948bc74dcd14
rebeccanoel <rnoel@mit.edu>
2017-08-09 16:19:02 -0400

    Add files via upload

--- test_function.py
+++ test_function.py
@@ -4,4 +4,5 @@ test function for purpose of running open in mind
 
 def test_function(exp_number):
 	experiments = [1,2,3,4,5]
+	print("hello")
 	print("this is a test funciton for running experiment ", experiments[exp_number], "in OpenMind.")
