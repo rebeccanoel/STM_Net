a7473248e54ca3cd56aa852aa33edbfb6995550e
rebeccanoel <rnoel@mit.edu>
2017-08-09 16:24:20 -0400

    Add files via upload

--- test_function.py
+++ test_function.py
@@ -1,8 +1,9 @@
 '''
 test function for purpose of running open in mind
 '''
+import sys
+exp_number = sys.argv[1]
 
-def test_function(exp_number):
-	experiments = [1,2,3,4,5]
-	print("hello")
-	print("this is a test funciton for running experiment ", experiments[exp_number], "in OpenMind.")
+experiments = [1,2,3,4,5]
+print("hello")
+print("this is a test funciton for running experiment ", experiments[exp_number], "in OpenMind.")
